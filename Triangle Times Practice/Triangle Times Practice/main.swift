a5f66bde55d4e7332ffcb370c8d52c0a2d71214f
RSGC-dubczak-m <student@MacBook-Pro-0446-MDubczak.local>
2016-05-27 10:06:36 -0400

    Created Triangle Times Practice

---
+++ Triangle Times Practice/Triangle Times Practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Triangle Times Practice
+//
+//  Created by Student on 2016-05-27.
+//  Copyright © 2016 Maxym Dubczak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
